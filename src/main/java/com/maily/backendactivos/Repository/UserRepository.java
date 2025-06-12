4b742d2fa95e82cccdc64b0dba2c6b6b98ff3f2a
0000MAILY1111 <valdezmelizza19@gmail.com>
2025-06-12 17:06:06 -0400

    nuew

--- src/main/java/com/maily/backendactivos/Repository/UserRepository.java
+++ src/main/java/com/maily/backendactivos/Repository/UserRepository.java
@@ -3,7 +3,9 @@ package com.maily.backendactivos.Repository;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.maily.backendactivos.Model.User;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends MongoRepository<User, Long> {
     User findByEmail(String email);
     Boolean existsByEmail(String email);
